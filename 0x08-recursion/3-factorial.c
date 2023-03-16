a77bdf447c8ff77cb6357393ffe020e803b621be
jolaadeadekunle <omojolaadeadekunle@gmail.com>
2023-03-16 15:52:25 -0700

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,6 +6,9 @@
  *
  *Return:factorial of n, -1 if n is lower than 0
  */
+int factorial(int n)
+{
+
 if (n < 0)
 	return (-1);
 if (n == 0)
@@ -13,5 +16,3 @@ if (n == 0)
 
 return (n * factorial(n - 1));
 }
-}
-
