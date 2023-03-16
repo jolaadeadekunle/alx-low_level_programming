be9d1d058b83abe22b91149ffe3a1947f55d664c
jolaadeadekunle <omojolaadeadekunle@gmail.com>
2023-03-16 15:44:05 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *factorial-calcualte the factorial of a given number
+ *@n:given number
+ *
+ *Return:factorial of n, -1 if n is lower than 0
+ */
+if (n < 0)
+	return (-1);
+if (n == 0)
+	return (1)
+
+return (n * factorial(n - 1));
+}
+}
+
