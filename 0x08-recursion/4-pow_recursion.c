d141a1b49d04d2f5555f3e3fe265acf63918b3fc
jolaadeadekunle <omojolaadeadekunle@gmail.com>
2023-03-16 16:52:13 -0700

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- *_pow_recursion-function that returns x raised to the power of y
+ *pow_recursion-function that returns x raised to the power of y
  *@x: base number
  *@y: power number
  *Return:x raised to the power of y
